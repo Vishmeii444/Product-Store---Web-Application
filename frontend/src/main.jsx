c5099b33a8d2d86711d578740cd781874836b9a2
Vishmi <vishmihgangodawila@gmail.com>
2026-03-05 01:57:00 +0530

    Add React Router and page skeleton
    Add react-router-dom and basic routing: wrap the app with BrowserRouter in main.jsx and add Routes/Route usage in App.jsx for '/' and '/create'. Introduce placeholder pages (HomePage, CreatePage) and a new Navbar component. Remove several unused starter UI utilities and assets (color-mode, provider, toaster, tooltip, react.svg) and simplify App layout. Update package.json to include react-router-dom.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider} from "@/components/ui/provider";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "@/components/ui/provider";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
